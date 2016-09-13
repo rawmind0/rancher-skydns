c2c270b6515b4a960e888d308bb817e61a9c75a4
Raul Sanchez <rawmind@gmail.com>
2016-09-13 22:27:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+[![](https://images.microbadger.com/badges/image/rawmind/rancher-skydns.svg)](https://microbadger.com/images/rawmind/rancher-skydns "Get your own image badge on microbadger.com")
+
+rancher-skydns
+==============
+
+This image is the skydns dynamic conf for rancher. It comes from [rancher-tools][rancher-tools].
+
+## Build
+
+```
+docker build -t rawmind/rancher-skydns:<version> .
+```
+
+## Versions
+
+- `2.5.3` [(Dockerfile)](https://github.com/rawmind0/rancher-skydns/blob/2.5.3/README.md)
+
+
+## Usage
+
+This image has to be run as a sidekick of [alpine-skydns][alpine-skydns], and makes available /opt/tools volume. It scans from rancher-metadata, for a etcd stack and service, and generates etcd connection string dynamicly.
+
+
+[alpine-skydns]: https://github.com/rawmind0/alpine-kafka
+[rancher-tools]: https://github.com/rawmind0/rancher-tools
---
+++ root/opt/tools/confd/bin/gen.conf.tmpl.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+ETCD_PORT=${ETCD_PORT:-"2379"}
+
+cat << EOF > ${SERVICE_VOLUME}/confd/etc/conf.d/server.properties.toml
+[template]
+src = "skydns-source.tmpl"
+dest = "${SERVICE_HOME}/etc/skydns-source"
+owner = "${SERVICE_USER}"
+mode = "0644"
+keys = [
+  "/stacks",
+]
+EOF
+
+cat << EOF > ${SERVICE_VOLUME}/confd/etc/templates/skydns-source.tmpl
+#!/usr/bin/env bash
+
+export ETCD_MACHINES=
+{{- \$etcd_link := split (getenv "ETCD_SERVICE") "/" -}}
+{{- \$etcd_stack := index \$etcd_link 0 -}}
+{{- \$etcd_service := index \$etcd_link 1 -}} 
+{{- range \$i, \$e := ls (printf "/stacks/%s/services/%s/containers" \$etcd_stack \$etcd_service) -}}
+  {{- if \$i -}},{{- end -}}
+  http://{{- getv (printf "/stacks/%s/services/%s/containers/%s/primary_ip" \$etcd_stack \$etcd_service \$e)-}}:\${ETCD_PORT}
+{{- end -}}
+
+export ETCD_MACHINES=${ETCD_MACHINES:-"http://etcd:2379"}
+export SKYDNS_ADDR=${SKYDNS_ADDR:-"0.0.0.0:5353"}
+export SKYDNS_DOMAIN=${SKYDNS_DOMAIN:-"dev.local"}
+export SKYDNS_PATH_PREFIX=${SKYDNS_PATH_PREFIX:-"skydns"}
+export SKYDNS_NDOTS=${SKYDNS_NDOTS:-"1"}
+export SKYDNS_NO_REC=${SKYDNS_NO_REC:-"true"}
+if [ "${SKYDNS_NO_REC}" == "true" ]; then
+	export SKYDNS_NAMESERVERS=${SKYDNS_NAMESERVERS:-""} 
+else
+   	export SKYDNS_NAMESERVERS=${SKYDNS_NAMESERVERS:-"8.8.8.8:53,8.8.4.4:53"} 
+fi
+EOF
